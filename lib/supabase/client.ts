e0fdcc32a4866c3350aeaf73bd3f15d026687a55
xcdsqws <jslee01898@gmail.com>
2025-04-18 13:49:45 +0900

    zzz

--- lib/supabase/client.ts
+++
@@ -1,6 +0,0 @@
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
-import type { Database } from "@/lib/database.types"
-
-export const createClient = () => {
-  return createClientComponentClient<Database>()
-}
--- lib/supabase/server.ts
+++
@@ -1,7 +0,0 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
-import { cookies } from "next/headers"
-import type { Database } from "@/lib/database.types"
-
-export const createClient = () => {
-  return createServerComponentClient<Database>({ cookies })
-}
